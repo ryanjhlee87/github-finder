2448c1b1705a3f4e2dec4b1b606ff89aaf12b45d
ryanjhlee87 <ryan0873035734@gmail.com>
2022-10-26 17:16:00 +0900

    Add user page

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -3,77 +3,21 @@ import githubReducer from './GithubReducer';
 
 const GithubContext = createContext();
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
-
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
   };
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  const searchUsers = async text => {
-    setLoading();
-
-    const params = new URLSearchParams({
-      q: text,
-    });
-    try {
-      const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-        headers: {
-          Authorization: `token ${GITHUB_TOKEN}`,
-        },
-      });
-
-      const { items } = await response.json();
-
-      dispatch({ type: 'GET_USERS', payload: items });
-    } catch (error) {
-      console.error(error.message);
-    }
-  };
-
-  const getUser = async login => {
-    setLoading();
-
-    try {
-      const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-        headers: {
-          Authorization: `token ${GITHUB_TOKEN}`,
-        },
-      });
-
-      if (response.status === 404) {
-        window.location = '/notfound';
-      } else {
-        const data = await response.json();
-
-        dispatch({ type: 'GET_USER', payload: data });
-      }
-    } catch (error) {
-      console.error(error.message);
-    }
-  };
-
-  const clearUsers = () => {
-    setLoading();
-    dispatch({ type: 'CLEAR_USERS' });
-  };
-
-  const setLoading = () => dispatch({ type: 'SET_LOADING' });
-
   return (
     <GithubContext.Provider
       value={{
-        user: state.user,
-        users: state.users,
-        loading: state.loading,
-        searchUsers,
-        clearUsers,
-        getUser,
+        ...state,
+        dispatch,
       }}
     >
       {children}
--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -7,10 +7,11 @@ const githubReducer = (state, action) => {
         loading: false,
       };
 
-    case 'GET_USER':
+    case 'GET_USER_AND_REPOS':
       return {
         ...state,
-        user: action.payload,
+        user: action.payload.user,
+        repos: action.payload.repos,
         loading: false,
       };
 
